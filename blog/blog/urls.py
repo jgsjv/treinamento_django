65605e4e13470dc8c4b7fb9359cf1dda8ae4e888
Arthur Alvim <afmalvim@gmail.com>
2015-11-17 18:19:57 -0300

    Treino 17/11/2015

---
+++ blog/blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^markdown/', include("django_markdown.urls")),
+    url(r'^lista/$', 'blogapp.views.blog_index', name='lista'),
+
+]
