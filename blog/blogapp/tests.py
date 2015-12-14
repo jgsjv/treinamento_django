bf1042a52e579f0456769383b5b902f6f5cd5f13
Arthur Alvim <afmalvim@gmail.com>
2015-12-14 13:31:13 -0300

    update nos testes

--- blog/blogapp/tests.py
+++ blog/blogapp/tests.py
@@ -7,6 +7,20 @@ from blogapp.models import Sponsors
 from blogapp.models import Speakers
 from .enum import LEVEL_CHOICES
 from .enum import GOLD, SILVER, BRONZE
+from django.core.urlresolvers import reverse
+from model_mommy import mommy
+from model_mommy.recipe import Recipe, foreign_key
+
+lista_de_urls = {
+        'home': '/home',
+        'patrocinadores': '/sponsor',
+        'palestrantes': '/speaker',
+        'noticias': '/news',
+        'evento': '/event',
+        'agradecimento': '/thanks',
+        'publicacao': '(?P<pk>[\d]+)',
+    }
+
 
 # Testes de criacao de models.
 
@@ -46,73 +60,84 @@ class SponsorTestCase(TestCase):
 
 class NewsIndexTestCase(TestCase):
 
+    fixtures = ['blogapp']
+
     def test_index(self):
         resp = self.client.get('/news')
         self.assertEqual(resp.status_code, 200)
-
+        self.assertTrue('news' in resp.context)
 
 # Voltar para esse aqui.
 
-# class PublicacoesDetalhesTestCase(TestCase):
 
-#   def test_index(self):
-#       resp = self.client.get('/(?P<pk>[\d]+)')
-#       self.assertEqual(resp.status_code, 200)
+class PublicacoesDetalhesTestCase(TestCase):
+
+    def test_index(self):
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('publicacao', args=(publicacao.pk,))
+        url_desejada = 'publicacoes/{0}/detalhes'.format(self.entries)
+        self.assertEqual(url, url_desejada)
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
 
 
 class BlogIndexTestCase(TestCase):
 
+    fixtures = ['blogapp']
+
     def test_index(self):
-        resp = self.client.get('/home')
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('home')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
 
 
 class SpeakerIndexTestCase(TestCase):
 
     def test_index(self):
-        resp = self.client.get('/speaker')
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('palestrantes')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+        self.assertTrue('speakers' in resp.context)
 
 
 class SponsorIndexTestCase(TestCase):
 
     def test_index(self):
-        resp = self.client.get('/sponsor')
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('patrocinadores')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+        self.assertTrue('sponsors' in resp.context)
 
 
 class EventoViewTestCase(TestCase):
 
     def test_index(self):
-        resp = self.client.get('/event')
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('evento')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
 
 
 class ThanksViewTestCase(TestCase):
 
     def test_index(self):
-        resp = self.client.get('/thanks')
+        for chave, url in lista_de_urls.iteritems():
+            url = reverse('agradecimento')
+        resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Testes de managers.
 
 # Testes de forms.
+
+
+# class EventoFormTestCase(TestCase):
+
+    # def test_good_contact(self):
+    #     post = EventoForm.object.get(pk=1)
+    #    resp = self.client.post('evento', {'subject': Testes, 'message': Comentario ou sugestao, 'sender': jgsjv@cin.ufpe.br})
+    #    self.assertEqual(resp.satus_code, 200)
 
